a09b4c0a0d4cf6fda761ff1662e3bbe669cb0dd4
Tarek Alabd <tarekalabd1@gmail.com>
2019-08-12 13:35:45 +0200

    the whole project

---
+++ lib/anim/counter_animation.dart
@@ -0,0 +1,28 @@
+
+import 'package:flutter/material.dart';
+
+class CounterAnimator extends StatefulWidget {
+  @override
+  _CounterAnimatorState createState() => _CounterAnimatorState();
+}
+
+class _CounterAnimatorState extends State<CounterAnimator> with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void initState() {
+    _controller = AnimationController(vsync: this);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+  
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
